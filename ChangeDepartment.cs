994d4930040d402a57fbcf94e8aa41e632170bc5
Алёна Крипец < >
2017-04-07 13:09:33 +0300

    added windows forms

---
+++ ChangeDepartment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sh_lb2_v6
+{
+    public partial class ChangeDepartment : Form
+    {
+        public ChangeDepartment()
+        {
+            InitializeComponent();
+        }
+    }
+}
