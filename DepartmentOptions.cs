c1191c8610b3741492b7abeb54442dda60fb21fb
Алёна Крипец < >
2017-04-07 14:09:59 +0300

    добавили пару форм

---
+++ DepartmentOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sh_lb2_v6
+{
+    public partial class DepartmentOptions : Form
+    {
+        public DepartmentOptions()
+        {
+            InitializeComponent();
+        }
+
+        private void AddDepartment_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
