0defa01308209caacbce03e7792910f7b42c83d6
Алёна Крипец < >
2017-04-07 15:04:36 +0300

    реализован класс Employee

--- Employee.cs
+++ Employee.cs
@@ -8,5 +8,19 @@ namespace sh_lb2_v6
 {
     class Employee
     {
+        public String FullName { private set; get; }
+        public Decimal Salary { private set; get; }
+        Employee(String _fullName, Decimal _salary)
+        {
+            if (_fullName.Length > 0)
+                FullName = _fullName;
+            else
+                throw new ArgumentException("Введено неккоректное имя.");
+
+            if (Salary > 0)
+                Salary = _salary;
+            else
+                throw new ArgumentException("Введена неккоректная оплата труда.");
+        }
     }
 }
