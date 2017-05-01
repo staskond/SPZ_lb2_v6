e2c860a91a75387b061a9cd8df3d3c2f9108d0ab
Алёна Крипец < >
2017-05-01 18:31:35 +0300

    started work at 01.05.2017

---
+++ StaticClassDep.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sh_lb2_v6
+{
+    public static class StaticClassDep
+    {
+        public static string nameDep { get; set; }
+        public static int maxEmployee { get; set; }
+        public static string ManagerName { get; set; }
+    }
+}
