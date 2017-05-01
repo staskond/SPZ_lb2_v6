39c681b1c03040ebff8d4fffa600af8c2517fbc3
Алёна Крипец < >
2017-05-01 21:48:44 +0300

    работает

---
+++ StaticClassEmployee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sh_lb2_v6
+{
+    public static class StaticClassEmployee
+    {
+        public static string FullName { set; get; }
+        public static decimal Salary { set; get; }
+        public static void NulableAllValue()
+        {
+            FullName = null;
+            Salary = 0;
+        }
+    }
+}
